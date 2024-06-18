90c0dc8ab4ac5664ad9cecd9619e9e809e8261c5
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2024-06-17 17:53:49 -0700

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { sveltekit } from '@sveltejs/kit/vite';
+import Icons from 'unplugin-icons/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		purgeCss(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
+	server: {
+		hmr: false
+	}
+});
