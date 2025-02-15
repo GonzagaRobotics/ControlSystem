51a7fbc8ebc3897baab3ca5456d3ce1a39f025b0
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-02-15 22:55:18 +0100

    Added placeholder gnss pane

--- src/lib/components/panes/paneList.ts
+++ src/lib/components/panes/paneList.ts
@@ -1,6 +1,7 @@
 import Arm from './Arm.svelte';
 import AutoNav from './AutoNav.svelte';
 import Camera from './Camera.svelte';
+import GNSS from './GNSS.svelte';
 import Motors from './Motors.svelte';
 import Science from './Science.svelte';
 import Unknown from './Unknown.svelte';
@@ -9,6 +10,7 @@ export const paneList = {
 	unknown: Unknown,
 	camera: Camera,
 	motors: Motors,
+	gnss: GNSS,
 	arm: Arm,
 	autoNav: AutoNav,
 	science: Science
