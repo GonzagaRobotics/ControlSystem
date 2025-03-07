decc322324757abb85afb0c52aca56f230e8fc5f
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-03-07 17:34:04 +0100

    v0.14 - cameras work with camera system v0.3, updated dependencies

--- src/lib/components/panes/paneList.ts
+++ src/lib/components/panes/paneList.ts
@@ -1,6 +1,6 @@
 import Arm from './Arm.svelte';
 import AutoNav from './AutoNav.svelte';
-import Camera from './Camera.svelte';
+import Camera from './camera/Camera.svelte';
 import GNSS from './GNSS.svelte';
 import Motors from './Motors.svelte';
 import Science from './Science.svelte';
