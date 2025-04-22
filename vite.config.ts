1f324e9ab58acfb5ae8104327f0c377907d47fe9
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-04-22 19:05:58 +0200

    Full reload on HMR

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, ModuleNode, type PluginOption } from 'vite';
+
+const fullReloadAlways: PluginOption = {
+	name: 'full-reload',
+	handleHotUpdate({ server, modules, timestamp }) {
+		const invalidatedModules = new Set<ModuleNode>();
+
+		for (const mod of modules) {
+			server.moduleGraph.invalidateModule(mod, invalidatedModules, timestamp, true);
+		}
+
+		server.ws.send({ type: 'full-reload' });
+
+		return [];
+	}
+};
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), fullReloadAlways],
 	test: {
 		workspace: [
 			{
