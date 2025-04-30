6d27a481c4d1dd19c5cf9066f79b52be41ca3c67
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-04-30 20:21:47 +0200

    Camera. Still need to contain video element

--- src/lib/components/panes/camera/Camera.svelte
+++ src/lib/components/panes/camera/Camera.svelte
@@ -1,7 +1,81 @@
 <script lang="ts">
+	import { getContext, onMount } from 'svelte';
 	import Pane from '../Pane.svelte';
+	import type { Core } from '$lib/core/core.svelte';
+	import { RTC } from './rtc';
+	import { Popover } from '@skeletonlabs/skeleton-svelte';
+	import { Video } from '@lucide/svelte';
 
 	let { id, start, size } = $props();
+
+	const core = getContext<Core>('core');
+	const rtc = new RTC(`ws://${core.config.rtcSignalingUrl}`);
+
+	let video: HTMLVideoElement;
+
+	const sources = rtc.sources;
+	let selectedSource = $state('');
+
+	let selectOpen = $state(false);
+
+	$effect(() => {
+		if (selectedSource != '') {
+			rtc.connectToSource(selectedSource);
+		}
+	});
+
+	$effect(() => {
+		if ($sources.find((source) => source === selectedSource) || selectedSource == '') {
+			return;
+		}
+
+		selectedSource = '';
+		core.sendToast('warning', 'Your selected source is no longer available');
+	});
+
+	onMount(() => {
+		rtc.pc.subscribe((current) => {
+			// Remove previous video stream
+			video.srcObject = null;
+
+			current.ontrack = (event) => {
+				video.srcObject = event.streams[0];
+			};
+		});
+	});
 </script>
 
-<Pane {id} {start} {size}></Pane>
+<Pane {id} {start} {size}>
+	<Popover
+		open={selectOpen}
+		onOpenChange={(e) => (selectOpen = e.open)}
+		positioning={{ placement: 'bottom-end' }}
+		triggerBase="relative float-right mr-4"
+		triggerClasses={selectedSource === '' ? 'animate-pulse' : ''}
+		contentBase="card bg-surface-200-800 p-1"
+	>
+		{#snippet trigger()}
+			<Video size="2rem" />
+		{/snippet}
+
+		{#snippet content()}
+			<ul class="list">
+				{#each $sources as source}
+					<li>
+						<button
+							class="btn-sm hover:preset-filled-primary-500 transition-all"
+							class:preset-filled-secondary-500={selectedSource === source}
+							onclick={() => (selectedSource = source)}
+						>
+							{source}
+						</button>
+					</li>
+				{:else}
+					<li>No sources found</li>
+				{/each}
+			</ul>
+		{/snippet}
+	</Popover>
+
+	<video bind:this={video} autoplay muted></video>
+</Pane>
---
+++ src/lib/components/panes/camera/rtc.ts
@@ -0,0 +1,145 @@
+import { CONTROL_SOURCE_NAME } from '$lib/core/core.svelte';
+import { get, writable, type Readable, type Writable } from 'svelte/store';
+
+export class RTC {
+	/** The websocket to the signaling server. */
+	readonly socket: WebSocket;
+
+	private _pc: Writable<RTCPeerConnection>;
+	private _sources: Writable<string[]>;
+	private _makingOffer: boolean;
+
+	public get pc(): Readable<RTCPeerConnection> {
+		return this._pc;
+	}
+
+	public get sources(): Readable<string[]> {
+		return this._sources;
+	}
+
+	constructor(url: string) {
+		this._pc = writable(new RTCPeerConnection());
+		this._sources = writable([]);
+		this.socket = new WebSocket(`${url}/${CONTROL_SOURCE_NAME}`);
+		this._makingOffer = false;
+
+		this.setup();
+	}
+
+	async connectToSource(source: string) {
+		this.socket.send(JSON.stringify({ type: 'sourceChange', to: source }));
+
+		const pc = get(this._pc);
+
+		const offer = await pc.createOffer();
+		await pc.setLocalDescription(offer);
+
+		console.log('|Camera| Sending offer');
+
+		this.socket.send(JSON.stringify(offer));
+	}
+
+	reset() {
+		this._pc.set(new RTCPeerConnection());
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	private async onMessage(message: any) {
+		switch (message.type) {
+			case 'sources': {
+				console.log('|Camera| Got new sources');
+				this._sources.set(message.sources);
+				break;
+			}
+			case 'candidate': {
+				console.log('|Camera| Got candidate');
+				get(this._pc).addIceCandidate(message.candidate);
+				break;
+			}
+			default: {
+				const pc = get(this._pc);
+
+				// As the impolite peer, we ignore offers during a collision
+				if (message.type === 'offer' && (this._makingOffer || pc.signalingState !== 'stable')) {
+					return;
+				}
+
+				await pc.setRemoteDescription(message);
+
+				if (message.type == 'offer') {
+					console.log('|Camera| Got offer');
+
+					await pc.setLocalDescription();
+					this.socket.send(JSON.stringify(pc.localDescription));
+				} else if (message.type == 'answer') {
+					console.log('|Camera| Got answer');
+				} else {
+					console.log('|Camera| Unknown message type:', message.type);
+				}
+			}
+		}
+	}
+
+	private setup() {
+		const pc = get(this._pc);
+
+		this.setupLogListeners();
+
+		pc.onicecandidate = (event) => {
+			if (event.candidate) {
+				this.socket.send(JSON.stringify({ type: 'candidate', candidate: event.candidate }));
+			}
+		};
+
+		pc.onnegotiationneeded = async () => {
+			console.log(`|Camera| Negotiation needed`);
+
+			try {
+				this._makingOffer = true;
+				const offer = await pc.createOffer();
+				await pc.setLocalDescription(offer);
+				this.socket.send(JSON.stringify(offer));
+			} catch (err) {
+				console.error(err);
+			} finally {
+				this._makingOffer = false;
+			}
+		};
+
+		this.socket.onmessage = (ev) => {
+			this.onMessage(JSON.parse(ev.data));
+		};
+
+		this.socket.onclose = () => {
+			console.log('|Camera| WebSocket closed');
+		};
+
+		this.socket.onerror = (event) => {
+			console.error('|Camera| WebSocket error', event);
+		};
+	}
+
+	private setupLogListeners() {
+		const pc = get(this._pc);
+
+		pc.onsignalingstatechange = () => {
+			console.log(`|Camera| Signaling state: ${pc.signalingState}`);
+		};
+
+		pc.oniceconnectionstatechange = () => {
+			console.log(`|Camera| ICE connection state: ${pc.iceConnectionState}`);
+		};
+
+		pc.onicegatheringstatechange = () => {
+			console.log(`|Camera| ICE gathering state: ${pc.iceGatheringState}`);
+		};
+
+		pc.onconnectionstatechange = () => {
+			console.log(`|Camera| Connection state: ${pc.connectionState}`);
+		};
+
+		pc.onicecandidateerror = (event) => {
+			console.error('|Camera| ICE candidate error:', event);
+		};
+	}
+}
